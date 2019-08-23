d44f9fea7fe06e7968c1cb39e5a99347c8ac620c
yangpan <473075889@qq.com>
2019-08-23 13:06:09 +0800

    finally

--- src/main/java/com/tzauto/entity/RelationEntity.java
+++ src/main/java/com/tzauto/entity/RelationEntity.java
@@ -6,8 +6,9 @@ package com.tzauto.entity;
 public class RelationEntity {
     public RelationEntity() {
     }
-//iSecsHost.executeCommand("dialog \"Lot No\" write " + lotId);
-    public RelationEntity(Integer id, String lot, String materialNumber, String recipeName,String fixtureno) {
+
+    //iSecsHost.executeCommand("dialog \"Lot No\" write " + lotId);
+    public RelationEntity(Integer id, String lot, String materialNumber, String recipeName, String fixtureno) {
         this.id = id;
         this.lot = lot;
         this.materialNumber = materialNumber;
@@ -70,4 +71,44 @@ public class RelationEntity {
     public void setRecipeName(String recipeName) {
         this.recipeName = recipeName;
     }
+
+    public static void main(String[] args) {
+        String str = stringToAscii("输入文本");
+        System.out.println(str);
+    }
+
+    /**
+     * 字符串转换为Ascii
+     *
+     * @param value
+     * @return
+     */
+    public static String stringToAscii(String value) {
+        StringBuffer sbu = new StringBuffer();
+        char[] chars = value.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if (i != chars.length - 1) {
+                sbu.append((int) chars[i]).append(",");
+            } else {
+                sbu.append((int) chars[i]);
+            }
+        }
+        return sbu.toString();
+    }
+
+    /**
+     * Ascii转换为字符串
+     *
+     * @param value
+     * @return
+     */
+    public static String asciiToString(String value) {
+        StringBuffer sbu = new StringBuffer();
+        String[] chars = value.split(",");
+        for (int i = 0; i < chars.length; i++) {
+            sbu.append((char) Integer.parseInt(chars[i]));
+        }
+        return sbu.toString();
+    }
+
 }
