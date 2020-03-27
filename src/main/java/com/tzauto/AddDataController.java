1f9467d6d5bd501bd7d5cb5954726be74a260ec4
杨攀 <473075889>
2020-03-27 09:58:32 +0800

    修改界面样式，以及自主检改为单选框

--- src/main/java/com/tzauto/AddDataController.java
+++ src/main/java/com/tzauto/AddDataController.java
@@ -5,7 +5,9 @@ import com.tzauto.server.MainServer;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.net.URL;
@@ -18,7 +20,9 @@ public class AddDataController implements Initializable {
     @FXML
     private TextField lot;
     @FXML
-    private TextField isMain;
+    private RadioButton isMain;
+    @FXML
+    private RadioButton notMain;
 
     @FXML
     private TextField item2;
@@ -28,26 +32,37 @@ public class AddDataController implements Initializable {
     private TextField item5;
     @FXML
     private TextField item6;
-
+    ToggleGroup tg;
     @Autowired
     MainServer mainServer;
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        tg = new ToggleGroup();
+        isMain.setSelected(true);
+        isMain.setToggleGroup(tg);
+        notMain.setToggleGroup(tg);
+//        tg.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+//            @Override
+//            public void changed(ObservableValue<? extends Toggle> changed, Toggle oldVal, Toggle newVal) {
+//                RadioButton temp_rb = (RadioButton) newVal;
+//                System.out.println(temp_rb.getText());
+//            }
+//        });
     }
 
 
     public void action(ActionEvent actionEvent) {
-
-        boolean b = mainServer.addData(lot.getText().trim(), item2.getText().trim(), item4.getText().trim(), item5.getText().trim(), item6.getText().trim(), isMain.getText().trim());
+        String selectedText = ((RadioButton) tg.getSelectedToggle()).getText();
+        boolean b = mainServer.addData(lot.getText().trim(), item2.getText().trim(), item4.getText().trim(), item5.getText().trim(), item6.getText().trim(), selectedText);
         if (b) {
+            lot.setText("");
             item2.setText("");
             item4.setText("");
             item5.setText("");
             item6.setText("");
-            isMain.setText("");
+            isMain.setSelected(true);
         }
     }
 
